ac5c2639eb45d63694efdc8921daf8e9a2dfb729
Ammar <ammarraneez@gmail.com>
2021-02-23 20:52:16 +0530

    trial backend host on firebase

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://maqbool-reg.herokuapp.com'
+    baseURL: 'maqbool-reg-backend.firebaseapp.com'
     // baseURL: 'http://localhost:9000'
 })
 
